525d677c8fe3e4485f59ef0a290f4566d34d98eb
hexoN <aliakbaripour00@gmail.com>
2024-03-26 20:23:06 +0330

    Added a simple script to toggle menu-on class

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuElement = document.querySelector('.icon');
+const menuDropdown = document.querySelector('.menu-dropdown')
+
+function displayDropdown() {
+  menuDropdown.classList.toggle('menu-on')
+}
+
+menuElement.addEventListener('click', () => displayDropdown)
