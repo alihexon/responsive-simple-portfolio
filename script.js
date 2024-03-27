1956b2c185531832bf21da3255ed5781143a7e6e
hexoN <aliakbaripour00@gmail.com>
2024-03-27 14:22:33 +0330

    Reworked the displayDropdown script

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const menuElement = document.querySelector('.icon');
 const menuDropdown = document.querySelector('.menu-dropdown');
 
 function displayDropdown() {
-  menuDropdown.classList.toggle('menu-on');
+  if (menuDropdown.classList.contains('menu-on')) {
+    menuDropdown.classList.remove('menu-on');
+    menuDropdown.classList.add('menu-off');
+  } else {
+    menuDropdown.classList.remove('menu-off');
+    menuDropdown.classList.add('menu-on');
+  }
 }
 
 menuElement.addEventListener('click', displayDropdown);
